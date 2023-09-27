c2197ea34c3548107f3ff4f4c6c1b17d8a694ba6
Kevin <30410827+DasBonDon@users.noreply.github.com>
2023-09-27 13:51:35 -0400

    Comments

--- Chapter 2/famous_quote.py
+++ Chapter 2/famous_quote.py
@@ -1,4 +1,4 @@
-#2-5 Famous Quote
+#2-5/2-6 Famous Quote
 name = "George S. Patton"
 quote = '"If everyone is thinking alike, someone isn’t thinking."'
 # I used an F-string to combine the name and quote variables.
--- Chapter 2/stripping_names.py
+++ Chapter 2/stripping_names.py
@@ -1,3 +1,4 @@
+#2-7 Stripping Names
 name = " kevin "
 # \n creates a new line while \t creates an indentation. The strip function is taking away the spaces on either side of the name variable.
 print(name, '\n', name.lstrip(), '\n\t', name.rstrip(), '\n', name.strip())

bcc7564f86ce96b8571487c22da6d5c943620579
Kevin <30410827+DasBonDon@users.noreply.github.com>
2023-09-27 20:06:54 -0400

    glossary_2

---
+++ Chapter 6/glossary_2.py
@@ -0,0 +1,17 @@
+#6-4 Glossary 2
+glossary = {
+    'glossary': 'A dictionary',
+    'dictionary': 'A list with key-value pairs',
+    'if-elif-else': 'Boolean expressions',
+    'for loop': 'Used for iterating over a sequence',
+    'conditional tests': 'True or False statements',
+    'k, v': 'Key, Value',
+    'list': 'A built-in Python sequence',
+    'slice': 'An object usually containing a portion of a sequence',
+    'function': 'A series of statements which returns some value to a caller',
+    'f-string': 'String literals prefixed with "f"',
+}
+
+for k, v in glossary.items():
+    print(f"\nTerm: {k}")
+    print(f"Definition: {v}")

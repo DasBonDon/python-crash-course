8cd272b231def5dab0ee087a08f06fabde91daf8
Kevin <30410827+DasBonDon@users.noreply.github.com>
2023-09-27 20:42:35 -0400

    rivers

---
+++ Chapter 6/rivers.py
@@ -0,0 +1,10 @@
+# 6-5 Rivers
+rivers = {
+    'nile': 'egypt',
+    'rhine': 'germany',
+    'volga': 'russia',
+}
+
+for k, v in rivers.items():
+    print(f"\nRiver: {k.title()}")
+    print(f"Country: {v.title()}")

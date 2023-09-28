da95656d6a8d52f84998457d2e6c57817cc02158
Kevin <30410827+DasBonDon@users.noreply.github.com>
2023-09-28 13:42:54 -0400

    polling

---
+++ Chapter 6/polling.py
@@ -0,0 +1,14 @@
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'rust',
+    'phil': 'python',
+    }
+
+people = ['jen', 'sarah', 'kevin', 'ethan', 'josh']
+
+for person in people:
+    if person in favorite_languages:
+        print(f"Thank you {person.title()}, for taking the poll!")
+    else:
+        print(f"{person.title()}, you are invited to take the poll!")

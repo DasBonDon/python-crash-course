cdaaf5e566e007340f07f29def74ec5242c67b20
Kevin <30410827+DasBonDon@users.noreply.github.com>
2023-09-29 17:22:00 -0400

    people

--- Chapter 6/people.py
+++ Chapter 6/people.py
@@ -1,30 +1,14 @@
 # 6-7 People
-people = {
-    'gmarshall': {
-        'first_name': 'George', 
-        'last_name': 'Marshall',
-        'age': 35,
-        'city': 'San Francisco',
-    },
+gmarshall = {'first_name': 'George', 'last_name': 'Marshall',
+             'age': 35, 'city': 'San Francisco'}
 
-    'akozak': {
-        'first_name': 'Alexander',
-        'last_name': 'Kozak',
-        'age': 28,
-        'city': 'Kiev',
-    },
+akozak = {'first_name': 'Alexander', 'last_name': 'Kozak', 
+          'age': 28, 'city': 'Kiev'}
 
-    'wmonroe': {
-        'first_name': 'William',
-        'last_name': 'Monroe',
-        'age': 25,
-        'city': 'Houston',
-    },
+wmonroe = {'first_name': 'William', 'last_name': 'Monroe',
+           'age': 25, 'city': 'Houston'}
 
-}
+people = [gmarshall, akozak, wmonroe]
 
-for k, v in people.items():
-    print(f"\nFirst Name: {v['first_name']}")
-    print(f"Last Name: {v['last_name']}")
-    print(f"Age: {v['age']}")
-    print(f"City: {v['city']}")
+for person in people:
+    print(person)

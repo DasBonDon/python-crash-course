344477a9df8b4f7855763cc59d5e76f46ec86563
Kevin <30410827+DasBonDon@users.noreply.github.com>
2023-10-01 21:02:04 -0400

    favorite_places

---
+++ Chapter 6/favorite_places.py
@@ -0,0 +1,10 @@
+# 6-9 Favorite Places
+favorite_places = {
+    'kevin': 'germany',
+    'ethan': 'italy',
+    'hunter': 'united states',
+}
+
+for key, value in favorite_places.items():
+    print(f"\nName: {key.title()}")
+    print(f"Favorite Place: {value.title()}")

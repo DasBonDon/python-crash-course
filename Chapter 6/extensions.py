420bdf50454fc18257fc9b4030c6ade036a994e8
Kevin <30410827+DasBonDon@users.noreply.github.com>
2023-10-03 15:59:25 -0400

    extensions

---
+++ Chapter 6/extensions.py
@@ -0,0 +1,11 @@
+# 6-12 Extensions
+favorite_numbers = {
+    'kevin': '42',
+    'madi': '7',
+    'ethan': '27',
+    'josh': '60',
+    'hunter': '30',
+}
+
+for key, value in favorite_numbers.items():
+    print(f"\n{key.title()}'s favorite number is {value}!")

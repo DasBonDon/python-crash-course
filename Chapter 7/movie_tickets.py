98e56ffc7d0199f9f93fceb6d2394c414951c616
Kevin <30410827+DasBonDon@users.noreply.github.com>
2023-10-05 11:01:21 -0400

    More efficient

--- Chapter 7/movie_tickets.py
+++ Chapter 7/movie_tickets.py
@@ -4,7 +4,7 @@ age = int(age)
 
 if age < 3:
     print("Your ticket is free.")
-elif age >= 3 and age <= 12:
+elif age < 13:
     print("Your ticket is $10.")
 else:
     print("Your ticket is $15.")

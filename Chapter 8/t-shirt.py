b8427aade3297f70b492af7399808c17c145e83e
Kevin <30410827+DasBonDon@users.noreply.github.com>
2023-10-09 15:24:35 -0400

    t-shirt

---
+++ Chapter 8/t-shirt.py
@@ -0,0 +1,10 @@
+# 8-3 T-Shirt
+def make_shirt(size, text):
+    text = (
+        f'The size of this t-shirt is {size} ' 
+        f'and it has the text "{text.title()}" on it.'
+    )
+    print(text)
+
+make_shirt('large', 'hello world')
+make_shirt(size = 'small', text = 'i love donuts')

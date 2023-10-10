ec2d3334895a75f0e20a00fc742be56ec5432c21
Kevin <30410827+DasBonDon@users.noreply.github.com>
2023-10-10 11:21:05 -0400

    cities

---
+++ Chapter 8/cities.py
@@ -0,0 +1,7 @@
+# 8-5 Cities
+def describe_city(name, country = 'the united states'):
+    print(f"{name.title()} is in {country.title()}.")
+
+describe_city('new york')
+describe_city('detroit')
+describe_city('berlin', 'germany')

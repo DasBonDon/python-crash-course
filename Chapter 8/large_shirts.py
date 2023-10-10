4edceb95395feb721261bd2a86f01e6d0dfdfe94
Kevin <30410827+DasBonDon@users.noreply.github.com>
2023-10-10 11:18:47 -0400

    large_shirts

--- Chapter 8/large_shirts.py
+++ Chapter 8/large_shirts.py
@@ -1,10 +1,10 @@
 # 8-4 Large Shirts
 def make_shirt(size = 'large', text = 'i love python'):
-    text = (
+    message = (
         f'The size of this t-shirt is {size} ' 
         f'and it has the text "{text.title()}" on it.'
     )
-    print(text)
+    print(message)
 
 make_shirt()
 make_shirt(size = 'medium')

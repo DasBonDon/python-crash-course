cdf2ffa173305d9c56e9521ecba4043c87eeec29
Kevin <30410827+DasBonDon@users.noreply.github.com>
2023-10-12 09:33:31 -0400

    city_names

---
+++ Chapter 8/city_names.py
@@ -0,0 +1,7 @@
+# 8-6 City Names
+def city_country(city, country):
+    print(f"{city.title()}, {country.title()}")
+
+city_country('trenton', 'united states')
+city_country('munich', 'germany')
+city_country('moscow', 'russia')

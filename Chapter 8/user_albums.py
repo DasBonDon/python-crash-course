53655c666b941006b93b2054acaf80b23d879739
Kevin <30410827+DasBonDon@users.noreply.github.com>
2023-10-12 09:52:28 -0400

    user_albums

---
+++ Chapter 8/user_albums.py
@@ -0,0 +1,21 @@
+# 8-8 User Albums
+def make_album(artist, title, songs = None):
+    album = {'artist': artist, 'album': title}
+    if songs:
+        album['songs'] = int(songs)
+    return album
+
+while True:
+    print("\nPlease tell me your favorite album:")
+    print("(enter 'q' at any time to quit)")
+
+    artist = input("Artist: ")
+    if artist == 'q':
+        break
+
+    album = input('Album: ')
+    if album == 'q':
+        break
+
+    album = make_album(artist, album)
+    print(album)

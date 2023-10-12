c5bd5ae8ecb96cc1dd59af05a0355571b304775c
Kevin <30410827+DasBonDon@users.noreply.github.com>
2023-10-12 09:43:48 -0400

    album

---
+++ Chapter 8/album.py
@@ -0,0 +1,15 @@
+# 8-7 Album
+def make_album(artist, title, songs = None):
+    album = {'artist': artist, 'album': title}
+    if songs:
+        album['songs'] = int(songs)
+    return album
+
+album = make_album('led zeppelin', 'physical graffiti')
+print(album)
+album = make_album('steely dan', 'aja')
+print(album)
+album = make_album('herbie hancock', 'head hunters')
+print(album)
+album = make_album('mac demarco', 'salad days', '11')
+print(album)

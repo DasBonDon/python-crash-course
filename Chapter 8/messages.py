52533ae62e6ae64ba61e6b0c7f32c5e2263d5104
Kevin <30410827+DasBonDon@users.noreply.github.com>
2023-10-13 09:06:25 -0400

    messages

---
+++ Chapter 8/messages.py
@@ -0,0 +1,8 @@
+# 8-9 Messages
+texts = ['Hello.', 'How are you?', 'Do you want to go to the movies?']
+
+def show_messages(messages):
+    for message in messages:
+        print(message)
+
+show_messages(texts)

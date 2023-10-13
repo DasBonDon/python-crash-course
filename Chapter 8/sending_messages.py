18781ea9c125527632f35a4610133dbb8b47b93d
Kevin <30410827+DasBonDon@users.noreply.github.com>
2023-10-13 09:12:34 -0400

    sending_messages

---
+++ Chapter 8/sending_messages.py
@@ -0,0 +1,14 @@
+# 8-10 Sending Messages
+unsent_messages = ['Hello.', 'How are you?', 'Do you want to go to the movies?']
+sent_messages = []
+
+def show_messages(messages):
+    while unsent_messages:
+        current_message = unsent_messages.pop()
+        print(current_message)
+        sent_messages.append(current_message)
+
+show_messages(unsent_messages)
+
+print(unsent_messages)
+print(sent_messages)

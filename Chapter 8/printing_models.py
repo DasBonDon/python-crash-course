489e9537d5ea37a4bb305bca8af2d12c8851f05b
Kevin <30410827+DasBonDon@users.noreply.github.com>
2023-10-14 10:49:02 -0400

    printing_models

---
+++ Chapter 8/printing_models.py
@@ -0,0 +1,8 @@
+# 8-15 Printing Models
+import printing_functions as pf
+
+unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+
+pf.printing_models(unprinted_designs, completed_models)
+pf.show_completed_models(completed_models)

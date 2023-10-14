36842fe2aa2e48a5c64f37208a0d2983f5868b8f
Kevin <30410827+DasBonDon@users.noreply.github.com>
2023-10-14 10:57:57 -0400

    imports

---
+++ Chapter 8/imports.py
@@ -0,0 +1,11 @@
+# 8-16 Imports
+import message_functions
+from message_functions import display_message
+from message_functions import display_message as dm
+import message_functions as mf
+from message_functions import *
+
+# display_message() works for 1, 2 and 5
+display_message()
+dm()
+mf.display_message()

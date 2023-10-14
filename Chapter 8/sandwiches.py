343cd8287925b0a42a7438924eb3a8aea48e94b5
Kevin <30410827+DasBonDon@users.noreply.github.com>
2023-10-14 10:04:09 -0400

    sandwiches

---
+++ Chapter 8/sandwiches.py
@@ -0,0 +1,9 @@
+# 8-12 Sandwiches
+def make_sandwich(*toppings):
+    # Print out all the toppings
+    print(toppings)
+
+# Call the function
+make_sandwich('chicken', 'mayo', 'cheese')
+make_sandwich('ham,', 'cheese')
+make_sandwich('cheese')

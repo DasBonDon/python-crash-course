720723f6f5f596c1abef8034e5b3ca5897eb9815
Kevin <30410827+DasBonDon@users.noreply.github.com>
2023-10-19 12:30:49 -0400

    login_attempts

---
+++ Chapter 9/login_attempts.py
@@ -0,0 +1,34 @@
+# 9-5 Login Attempts
+class User:
+    def __init__(self, first_name, last_name, username, location):
+        self.first_name = first_name.title()
+        self.last_name = last_name.title()
+        self.username = username
+        self.location = location.title()
+        self.login_attempts = 0
+
+    def describe_user(self):
+        print(f"\nFirst Name: {self.first_name}")
+        print(f"Last Name: {self.last_name}")
+        print(f"Username: {self.username}")
+        print(f"Location: {self.location}")
+        
+    def greet_user(self):
+        print(f"\nWelcome back, {self.username}!")
+
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+
+user = User('George', 'Marshall', 'gmarsh', 'United States')
+
+user.increment_login_attempts()
+user.increment_login_attempts()
+user.increment_login_attempts()
+
+print(user.login_attempts)
+
+user.reset_login_attempts()
+print(user.login_attempts)

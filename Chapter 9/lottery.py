a44ca108f877a22bae0aad6a60c477e2c5967914
Kevin <30410827+DasBonDon@users.noreply.github.com>
2023-10-22 12:46:20 -0400

    lottery

---
+++ Chapter 9/lottery.py
@@ -0,0 +1,14 @@
+# 9-14 Lottery
+from random import choice
+
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'a', 'b', 'c', 'd', 'e']
+
+winner = []
+print("Let's find out whose gonna win!")
+
+while len(winner) < 4:
+    pull = choice(list)
+    print(f"\tThere's a {pull}!")
+    winner.append(pull)
+
+print(f"\nThe winning ticket is: {winner}")

a45082ea726bbd4c4a75ecf7620af9e386ad40f2
Kevin <30410827+DasBonDon@users.noreply.github.com>
2023-10-25 11:16:02 -0400

    learning_c

--- Chapter 10/learning_c.py
+++ Chapter 10/learning_c.py
@@ -4,6 +4,5 @@ from pathlib import Path
 path = Path('Chapter 10/learning_python.txt')
 contents = path.read_text()
 
-lines = contents.splitlines()
-for line in lines:
+for line in contents.splitlines():
     print(line.replace('Python', 'C'))

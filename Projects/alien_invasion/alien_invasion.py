cb5794f28102a8aef000bda23d379ce9b4333fcf
Kevin <30410827+DasBonDon@users.noreply.github.com>
2023-11-29 12:25:28 -0500

    alien_invasion

--- Projects/alien_invasion/alien_invasion.py
+++ Projects/alien_invasion/alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     # Overall class to manage game assets and behavior.
@@ -21,6 +22,9 @@ class AlienInvasion:
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
 
         # Set the background color.
         self.bg_color = (230, 230, 230)
@@ -80,12 +84,28 @@ class AlienInvasion:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
 
+    def _create_fleet(self):
+        # Create the fleet of aliens.
+        # Create an alien and keep adding aliens until there's no room left.
+        # Spacing between aliens is one alien width.
+        alien = Alien(self)
+        alien_width = alien.rect.width
+
+        current_x = alien_width
+        while current_x < (self.settings.screen_width - 2 * alien_width):
+            new_alien = Alien(self)
+            new_alien.x = current_x
+            new_alien.rect.x = current_x
+            self.aliens.add(new_alien)
+            current_x += 2 * alien_width
+
     def _update_screen(self):
          # Update images on the screen, and flip to the new screen.
             self.screen.fill(self.settings.bg_color)
             for bullet in self.bullets.sprites():
                  bullet.draw_bullet()
             self.ship.blitme()
+            self.aliens.draw(self.screen)
 
             pygame.display.flip()
 
